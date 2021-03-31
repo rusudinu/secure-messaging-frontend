132b8c1cdc1ad9f878cd72477313c366ec81f2f3
Rusu Dinu <dinu@codingshadows.com>
2021-03-31 07:50:19 +0300

    added the stomp dart client dependency

---
+++ lib/data/BackendServer.dart
@@ -0,0 +1,4 @@
+class BackendServer{
+  static String serverDomainName = "http://localhost:8089";
+//public static String serverDomainName = "https://securemessaging.codingshadows.com";
+}
--- lib/model/BackendServer.java
+++
@@ -1,6 +0,0 @@
-package model;
-
-public class BackendServer {
-    public static String serverDomainName = "http://localhost:8089";
-    //public static String serverDomainName = "https://securemessaging.codingshadows.com";
-}
